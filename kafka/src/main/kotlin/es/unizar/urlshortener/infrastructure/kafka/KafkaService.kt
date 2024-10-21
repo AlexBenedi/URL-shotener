9528d59029fe0f997d13b57e168d5bcd72bc8b2a
Juan Almodóvar <almodovar.juan7@gmail.com>
2024-10-21 21:06:42 +0200

    kafka tests & additional doc
    - testing both producer & consumer, in a mocked kafka server using mockito.
    - additional documentation which describes kafka example

--- kafka/src/main/kotlin/es/unizar/urlshortener/infrastructure/kafka/KafkaService.kt
+++ kafka/src/main/kotlin/es/unizar/urlshortener/infrastructure/kafka/KafkaService.kt
@@ -3,6 +3,7 @@ package es.unizar.urlshortener.infrastructure.kafka
 import org.springframework.kafka.core.KafkaTemplate
 import org.springframework.kafka.annotation.KafkaListener
 import org.springframework.stereotype.Service
+import java.util.concurrent.CountDownLatch
 
 // Class which defines kafka producer service
 @Service
@@ -18,11 +19,17 @@ class KafkaProducerService(private val kafkaTemplate: KafkaTemplate<String, Stri
 @Service
 class KafkaConsumerService {
     var lastReceivedMessage: String? = null
+    var latch = CountDownLatch(1)
 
-    // Listens to the messages sent to the specified topic and group
+    /*
+     * Consumes a message from the specified topic and prints it.
+     * As it is an example, it only prints the message and stores it in the lastReceivedMessage variable, 
+     * and only listens to the 'test' topic.
+     */
     @KafkaListener(topics = ["test"], groupId = "group_id")
     fun consume(message: String) {
         println("Consumed message: $message")
         lastReceivedMessage = message
+        latch.countDown()
     }
 }
--- kafka/src/test/kotlin/es/unizar/urlshortener/infrastructure/kafka/KafkaServiceTest.kt
+++ kafka/src/test/kotlin/es/unizar/urlshortener/infrastructure/kafka/KafkaServiceTest.kt
@@ -2,10 +2,14 @@
 
 package es.unizar.urlshortener.infrastructure.kafka
 
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.mockito.ArgumentCaptor
 import org.mockito.Mockito.*
 import org.springframework.kafka.core.KafkaTemplate
+import java.util.concurrent.CountDownLatch
+import java.util.concurrent.TimeUnit
 
 class KafkaServiceTest {
 
@@ -13,6 +17,14 @@ class KafkaServiceTest {
                 = mock(KafkaTemplate::class.java) as KafkaTemplate<String, String>
     private val kafkaProducerService = KafkaProducerService(kafkaTemplate)
 
+    private val kafkaConsumerService = KafkaConsumerService()
+
+    @BeforeEach
+    fun setUp() {
+        reset(kafkaTemplate)
+    }
+    
+    // Test the KafkaProducerService, which sends a message to Kafka
     @Test
     fun `test sending message to Kafka`() {
         // Arrange
@@ -22,16 +34,66 @@ class KafkaServiceTest {
         // Act
         kafkaProducerService.sendMessage(topic, message)
 
-        // Assert
-        val argumentCaptor = ArgumentCaptor.forClass(String::class.java)
-        verify(kafkaTemplate).send(argumentCaptor.capture(), argumentCaptor.capture())
+        // Argument captor to capture the topic and message sent to Kafka
+        val topicCaptor = ArgumentCaptor.forClass(String::class.java)
+        val messageCaptor = ArgumentCaptor.forClass(String::class.java)
+        verify(kafkaTemplate).send(topicCaptor.capture(), messageCaptor.capture())
 
         // Verify that the message was sent to the correct topic
-        println(argumentCaptor.allValues[0] + " " + argumentCaptor.allValues[1])
-        assert(argumentCaptor.allValues[0] == topic)
-        assert(argumentCaptor.allValues[1] == message)
+        assertEquals(topic, topicCaptor.value)
+        assertEquals(message, messageCaptor.value)
     }
-    /// TODO PROBAR RECIBIR MENSAJE DE KAFKA 
+
+    // Test the KafkaConsumerService, which consumes a message from Kafka
+    @Test
+    fun `test consuming message from Kafka`() {
+        // Arrange
+        val topic = "test"
+        val message = "Hello, Kafka!"
+        kafkaConsumerService.lastReceivedMessage = null
+        kafkaConsumerService.latch = CountDownLatch(1)
+
+        // Mock KafkaTemplate -> when a message is sent to Kafka, we call 'consume'
+        doAnswer {
+            kafkaConsumerService.consume(message)
+            null
+        }.`when`(kafkaTemplate).send(eq(topic), eq(message))
+
+        // Act
+        kafkaTemplate.send(topic, message)
+        kafkaConsumerService.latch.await(10, TimeUnit.SECONDS)
+
+        // Assert
+        assertEquals(message, kafkaConsumerService.lastReceivedMessage)
+    }
+    
+    /*
+     * Test the interaction between the KafkaProducerService and the KafkaConsumerService
+     * The producer sends a message to Kafka, and the consumer consumes it. 
+     * It is very similar to the previous test, but it uses both producer and consumer services.
+     */ 
+    @Test
+    fun `test producer and consumer interaction`() {
+        // Arrange
+        val topic = "test"
+        val message = "Hello, Kafka!"
+        kafkaConsumerService.lastReceivedMessage = null
+        kafkaConsumerService.latch = CountDownLatch(1)
+
+        // Mock KafkaTemplate -> when a message is sent to Kafka, we call 'consume'
+        doAnswer {
+            kafkaConsumerService.consume(message)
+            null
+        }.`when`(kafkaTemplate).send(eq(topic), eq(message))
+
+        // Act
+        kafkaProducerService.sendMessage(topic, message)
+        kafkaConsumerService.latch.await(10, TimeUnit.SECONDS)
+
+        // Assert
+        assertEquals(message, kafkaConsumerService.lastReceivedMessage)
+    }
+
     /// TODO TESTS TE INTEGRACIÓN CON EL SERVIDOR REAL DE KAFKA QUE CORRE EN DOCKER
     /// TODO REALIZAR UN MODO MANUAL CON UN ENDPOINT /kakfa QUE ENVÍE UN MENSAJE A KAFKA Y OTRO QUE LO RECIBA
     /// NO CORREN PRISA
