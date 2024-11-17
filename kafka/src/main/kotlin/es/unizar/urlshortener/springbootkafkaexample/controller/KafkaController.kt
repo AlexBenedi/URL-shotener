5d8c7d951e7694e4deccdd1e038a1d29dc574062
Juan Almodóvar <almodovar.juan7@gmail.com>
2024-11-17 21:11:58 +0100

    safety-check endpoint
    ============================================
    I'm using http endpoints which will be migrated to ws endpoints
    ============================================
    - safety-check endpoint (it is stub)

--- kafka/src/main/kotlin/es/unizar/urlshortener/springbootkafkaexample/controller/KafkaController.kt
+++ kafka/src/main/kotlin/es/unizar/urlshortener/springbootkafkaexample/controller/KafkaController.kt
@@ -2,6 +2,7 @@ package es.unizar.urlshortener.springbootkafkaexample.controller
 
 import es.unizar.urlshortener.springbootkafkaexample.service.KafkaProducerService
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
@@ -12,5 +13,11 @@ class KafkaController(private val kafkaProducerService: KafkaProducerService) {
     fun sendMessage(@RequestParam message: String): String {
         kafkaProducerService.sendMessage(message)
         return "Message sent successfully"
+    }
+
+    @PostMapping("/check-safety")
+    fun checkSafety(@RequestParam url: String): String {
+        kafkaProducerService.sendMessage(url)
+        return "Safety check requested"
     }
 }
