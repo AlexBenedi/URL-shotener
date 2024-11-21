668b770a7906eec3e053378a980092ff075d8542
Juan Almodóvar <almodovar.juan7@gmail.com>
2024-11-21 20:59:01 +0100

    Kafka & GSB Documentation

--- run.bat
+++ run.bat
@@ -1,5 +1,9 @@
 @echo off
 docker-compose down
 docker-compose up -d
-gradlew.bat build
-gradlew.bat bootrun
+call gradlew build >nul 2>&1 || goto :continue
+:continue
+call gradlew build >nul 2>&1 || goto :continue
+:continue
+call gradlew build
+call gradlew bootRun
