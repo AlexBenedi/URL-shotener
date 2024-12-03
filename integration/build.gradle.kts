44bb5cf6f15735e66d1be8e2e9ea1700b8c1edd1
AlexBenedi <843826@unizar.es>
2024-12-03 02:59:14 +0100

    Build integration

---
+++ integration/build.gradle.kts
@@ -0,0 +1,61 @@
+plugins {
+    // Apply the common conventions plugin for the URL shortener project
+    id("urlshortener-common-conventions")
+    id("org.sonarqube") version "6.0.1.5171"
+    id("jacoco")
+
+    // Apply the Kotlin JPA plugin
+    alias(libs.plugins.kotlin.jpa)
+
+    // Apply the Spring Boot plugin without automatically applying it
+    alias(libs.plugins.spring.boot) apply false
+
+    // Apply the Spring Dependency Management plugin
+    alias(libs.plugins.spring.dependency.management)
+}
+
+group = "es.unizar.webeng"
+version = "2024-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+kotlin {
+    jvmToolchain(17)
+}
+
+dependencies {
+
+    implementation(project(":gateway"))
+    implementation(project(":core"))
+    
+    implementation(platform("org.springframework.boot:spring-boot-dependencies:3.1.4"))
+    implementation("org.springframework.boot:spring-boot-starter-integration")
+    implementation("org.springframework.integration:spring-integration-stream")
+
+    implementation("org.springframework.boot:spring-boot-starter-integration")
+    implementation("org.springframework.integration:spring-integration-core")
+    
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.kafka:spring-kafka")
+    implementation("com.google.code.gson:gson:2.10.1")
+
+    runtimeOnly("org.springframework.boot:spring-boot-devtools")
+
+    compileOnly("org.projectlombok:lombok:1.18.28")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.springframework.kafka:spring-kafka-test")
+
+    detektPlugins("io.gitlab.arturbosch.detekt:detekt-formatting:1.23.0")
+}
+
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
+
+detekt {
+    autoCorrect = true
+}
