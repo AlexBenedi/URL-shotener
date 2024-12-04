9ad6b909265db8519dc54ea63295f704ed491241
AlexBenedi <843826@unizar.es>
2024-12-04 02:19:08 +0100

    Pruebas canal directo

--- integration/src/main/kotlin/es/unizar/urlshortener/integrations/api/PubSubMessageSender.kt
+++ integration/src/main/kotlin/es/unizar/urlshortener/integrations/api/PubSubMessageSender.kt
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Service
 
 import org.springframework.integration.channel.PublishSubscribeChannel
 
-
+/*
 @Service
 class PubSubMessageSender(private val pubSubChannel: MessageChannel) {
 
@@ -25,6 +25,29 @@ class PubSubMessageSender(private val pubSubChannel: MessageChannel) {
             .copyHeaders(headers)
             .build()
         pubSubChannel.send(message)
+        println("Mensaje enviado al canal: Payload=$payload, Headers=$headers")
+    }
+}
+*/
+
+
+
+@Service
+class PubSubMessageSender(private val direct: MessageChannel) {
+
+    fun sendQrMessage(payload: Any) {
+        sendMessage(payload, mapOf("type" to "qr"))
+    }
+
+    fun sendBrandedMessage(payload: Any) {
+        sendMessage(payload, mapOf("type" to "branded"))
+    }
+
+    private fun sendMessage(payload: Any, headers: Map<String, Any> = emptyMap()) {
+        val message = MessageBuilder.withPayload(payload)
+            .copyHeaders(headers)
+            .build()
+        direct.send(message)
         println("Mensaje enviado al canal: Payload=$payload, Headers=$headers")
     }
 }
