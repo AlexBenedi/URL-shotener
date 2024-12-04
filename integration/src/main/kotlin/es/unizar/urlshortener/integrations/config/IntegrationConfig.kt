e1e6e6b673a05ddbe6a310ac149f388c2238f0d0
AlexBenedi <843826@unizar.es>
2024-12-04 02:22:37 +0100

    Pruebas integrations

--- integration/src/main/kotlin/es/unizar/urlshortener/integrations/config/IntegrationConfig.kt
+++ integration/src/main/kotlin/es/unizar/urlshortener/integrations/config/IntegrationConfig.kt
@@ -15,13 +15,15 @@ import org.springframework.integration.dsl.IntegrationFlow
 import org.springframework.integration.dsl.MessageChannels
 import org.springframework.integration.dsl.Pollers
 import org.springframework.integration.dsl.PublishSubscribeChannelSpec
+import org.springframework.integration.dsl.DirectChannelSpec
+import org.springframework.integration.dsl.*
 import org.springframework.integration.dsl.integrationFlow
 import org.springframework.scheduling.annotation.EnableScheduling
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
 import java.util.Random
 import java.util.concurrent.atomic.AtomicInteger
-
+import org.springframework.integration.channel.DirectChannel
 import es.unizar.urlshortener.integration.api.PubSubMessageSender
 
 @Configuration
@@ -32,6 +34,65 @@ open class IntegrationConfig {
     @Bean
     open fun pubSubChannel(): PublishSubscribeChannelSpec<*> = MessageChannels.publishSubscribe()
 
+    @Bean
+    open fun direct(): DirectChannelSpec = MessageChannels.direct()
+
+    /* 
+    @Bean
+    open fun prueba(direct: DirectChannel): IntegrationFlow = integrationFlow(
+        source = { direct }
+    ) {
+        handle { payload: Any, headers: Map<String, Any> -> println("Prueba received: $payload") }
+    }
+    */
+    
+
+    @Bean
+    open fun routerFlow(direct: DirectChannel): IntegrationFlow = integrationFlow(
+        source = { direct }
+    ) {
+        route { message: Message<*> ->
+            val type = message.headers["type"] as? String
+            when (type) {
+                //"qr" -> "qrFlow"
+                "branded" -> "brandedFlow.input"
+                //else -> "defaultFlow"
+            }
+        } 
+    }
+
+    /*
+    @Bean
+    open fun qrFlow(): IntegrationFlow = integrationFlow("qrFlow") {
+        handle { payload: Any, headers: Map<String, Any> -> println("QR Consumer received: $payload") }
+    }
+    */
+
+    @Bean
+    open fun brandedFlow(): IntegrationFlow = integrationFlow("brandedFlow") {
+        handle { payload: Any, headers: Map<String, Any> -> println("Branded Consumer received: $payload") }
+    }
+
+    /*
+    @Bean
+    open fun defaultFlow(): IntegrationFlow = integrationFlow("defaultFlow") {
+        handle { payload: Any, headers: Map<String, Any> -> println("Default Consumer received: $payload") }
+    }
+    */
+
+    /*
+    @Bean
+    open fun brandedFlow(pubSubChannel: PublishSubscribeChannel): IntegrationFlow = integrationFlow(
+        source = { pubSubChannel }
+    ) {
+        filter { message: Message<*> -> message != null }
+        filter { message: Message<*> -> 
+            val type = message.headers["type"] as? String
+            type == "branded"
+        }
+        handle { payload: Any, headers: Map<String, Any> -> println("Branded Consumer received: $payload") }
+    }
+    */
 
     /*
     @Bean
@@ -57,3 +118,8 @@ open class IntegrationConfig {
         .get()
     */
 }
+
+data class Message<T>(
+    val payload: T, 
+    val headers: Map<String, Any>
+)
